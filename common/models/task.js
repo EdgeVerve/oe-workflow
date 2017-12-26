71fadeeefc67e838116855ad051cab7b8ad977c0
mandeep6ill <Mandeep_Gill@edgeverve.com>
2017-12-26 16:07:20 +0530

    allow everyone if candidate Users, Roles, Groups is not specified

--- common/models/task.js
+++ common/models/task.js
@@ -82,6 +82,12 @@ module.exports = function Task(Task) {
             resultData.push(instance);
             continue;
           } else {
+            // if the user was not excluded in any way
+            // and if candidateUsers, candidateRoles, &
+            // candidateGroups were not defined, assume it be candidate
+            if(candidateGroups.length === 0 && candidateRoles.length === 0 && candidateUsers.length === 0){
+              resultData.push(instance);
+            }
             continue;
           }
         }
