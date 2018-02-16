0c4d402f297ffc04aa87d449d8ef06446f0e695c
Prem.Sai <Prem.Sai@edgeverve.com>
2018-02-16 15:00:51 +0530

    Workflow recovery code merged with batch processing of process instances

--- server/boot/04-workflow-recovery.js
+++ server/boot/04-workflow-recovery.js
@@ -10,69 +10,114 @@
  */
 var logger = require('oe-logger');
 var log = logger('workflow-recovery.boot');
+var recoveryConfig = require('../wf-recovery-config.json');
+var EventEmitter = require('events');
+var emitter = new EventEmitter();
 
 module.exports = function attachWorkFlows(app) {
   var ProcessInstance = app.models.ProcessInstance;
+  var workflowMonitor = app.models.WorkflowMonitor;
   var BATCH_SIZE = 500;
   var BATCH_TIME = 100000;
+  var updateInterval = recoveryConfig.updateInterval;
+
   var options = {
     ctx: {},
     ignoreAutoScope: true,
     fetchAllScopes: true
   };
-
-  ProcessInstance.find({
-    where: {
-      'and': [{
-        '_status': 'running'
-      }, {
-        'passive-wait': false
-      }]
-    },
-    fields: {
-      'id': true
-    }
-  }, options, function fetchPendingPI(err, processes) {
+  // Trying to create an entry in the workflow monitor model
+  // This will ensure that this node will run the boot script and does the
+  // recovery process of all the process instances.
+  // If error in creating the entry because it is already existing , we will
+  // just return and will not continue to the boot recovery process.
+  workflowMonitor.create({"id": "node"}, options, function acquireLock(err, inst){
     if (err) {
       log.error(options, err);
       return;
+      // Check the error  
     }
+    ProcessInstance.find({
+      where: {
+        'and': [{
+          '_status': 'running'
+        }, {
+          'passive-wait': false
+        }]
+      },
+      fields: {
+        'id': true
+      }
+    }, options, function fetchPendingPI(err, processes) {
+      if (err) {
+        log.error(options, err);
+        return;
+      }
 
-    var buildFilter = function buildFilter(start, end) {
-      let filter = {
-        where: {
-          or: []
+      var buildFilter = function buildFilter(start, end) {
+        let filter = {
+          where: {
+            or: []
+          }
+        };
+        for (let idx = start; idx < end; idx++) {
+          filter.where.or.push({
+            id: processes[idx].id
+          });
         }
+        return filter;
       };
-      for (let idx = start; idx < end; idx++) {
-        filter.where.or.push({
-          id: processes[idx].id
-        });
-      }
-      return filter;
-    };
 
-    var NUM_PROCESSES = processes.length;
-    var iter = 0;
-    var interval = setInterval(function cb() {
-      let start = iter * BATCH_SIZE;
-      let end = (iter + 1) * BATCH_SIZE;
-      if (end >= NUM_PROCESSES) {
-        clearInterval(interval);
-        end = NUM_PROCESSES;
-      }
-
-      let filter = buildFilter(start, end);
-      ProcessInstance.find(filter, options, function fetchPendingPI(err, processes) {
-        if (err) {
-          log.error(options, err);
-          return;
+      var NUM_PROCESSES = processes.length;
+      var iter = 0;
+      var interval = setInterval(function cb() {
+        let start = iter * BATCH_SIZE;
+        let end = (iter + 1) * BATCH_SIZE;
+        if (end >= NUM_PROCESSES) {
+          clearInterval(interval);
+          end = NUM_PROCESSES;
         }
-        processes.forEach(process => {
-          process.recover();
+
+        let filter = buildFilter(start, end);
+        ProcessInstance.find(filter, options, function fetchPendingPI(err, processes) {
+          if (err) {
+            log.error(options, err);
+            return;
+          }
+          for (var i=0; i<processes.length; i++) {
+            var process = processes[i];
+            var processVersion = process._version;
+            setTimeout(function (process, ProcessInstance, options, processVersion){
+              return function () {
+                ProcessInstance.find({"where": {"id": process.id}}, options, function (err, pInstance){
+                  if (err) {
+                    log.error(options, err);
+                    return;
+                  }
+                  if (processVersion === pInstance[0]._version) {
+                    return pInstance[0].recover();
+                  } else if (processVersion !== pInstance[0]._version) {
+                    // Do nothing some other node is handling the process Instance.
+                  }
+                });
+
+              }
+            } (process, ProcessInstance, options, processVersion), updateInterval)
+          }
         });
-      });
-      iter += 1;
-    }, BATCH_TIME);
+        iter += 1;
+      }, BATCH_TIME);
+    });
+    setTimeout(function (inst, workflowMonitor, options) {
+      return function() {
+        workflowMonitor.destroyById(inst.id, options, function(err, inst) {
+          if (err) {
+            log.error(err);
+            return;
+          }
+        });
+      }
+    }(inst, workflowMonitor, options), 2*updateInterval);
   });
 };
+
