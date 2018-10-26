a3775d5a3e29c2f2599c1b2a57ee673da87cabbb
kvsrohit <kvsrohit@yahoo.com>
2018-10-26 12:21:30 +0530

    task hooks implemted

--- tools/cleandb.js
+++ tools/cleandb.js
@@ -7,16 +7,16 @@ var mongoPort = process.env.MONGO_PORT || 27017;
 
 var db = new Db(dbName, new Server(mongoHost, mongoPort));
 db.open(function (err, db) {
-    if (err) {
-        console.log("Error connecting to database", err);
-    } else {
-        db.dropDatabase(function(err, result) {
-            if (err) {
-                console.log("Error dropping database", err);
-            } else {
-                console.log("Database " + dbName + " has been dropped");
-                db.close();
-            }
-        });
-    }
+  if (err) {
+    console.log('Error connecting to database', err);
+  } else {
+    db.dropDatabase(function (err, result) {
+      if (err) {
+        console.log('Error dropping database', err);
+      } else {
+        console.log('Database ' + dbName + ' has been dropped');
+        db.close();
+      }
+    });
+  }
 });
