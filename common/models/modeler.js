66bd4d4c83ff9e176213c1e1ee4874bb838d1f58
Rohit Khode <Rohit.Khode@ad.infosys.com>
2019-09-27 07:44:40 +0530

    modeler APIs

---
+++ common/models/modeler.js
@@ -0,0 +1,41 @@
+
+/**
+ *
+ * ©2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
+ * Bangalore, India. All Rights Reserved.
+ *
+ */
+
+module.exports = function Modeler(Modeler) {
+
+  Modeler.flows = function(options, next){
+    var filter = { where: { 'latest': true }, fields: ['name'] };
+    Modeler.app.models.WorkflowDefinition.find(filter, options, 
+      function fetchWD(err, wfDefns) {
+      if (err) {
+        return next(err);
+      } else {
+        return next(null, wfDefns.map(item => item.name));
+      }
+    });
+  }
+
+  Modeler.remoteMethod('flows', {
+    description: 'Get flow list',
+    accessType: 'READ',
+    accepts: [{
+      arg: 'options',
+      type: 'object',
+      http: 'optionsFromRequest'
+    }],
+    http: {
+      verb: 'get',
+      path: '/flows'
+    },
+    returns: {
+      arg: 'data',
+      type: 'object',
+      root: true
+    }
+  });
+};
