9ac09cd69aac665ff04492811c236b97f938740d
vamsee <vamseerama_c@infosys.com>
2020-01-10 16:52:56 +0530

    removed unneeded packages and updated ci

--- test/oracle-utility.js
+++ test/oracle-utility.js
@@ -15,22 +15,29 @@
 // ORACLE_SYSUSER
 // ORACLE_SYSPASSWORD
 // ORACLE_SID
-// CI_PROJECT_NAME
 // CI_PROJECT_NAMESPACE
+// CI_PROJECT_NAME
 
 var oracledb = require('oracledb');
 var async = require('async');
+var fs = require('fs');
+var os = require('os');
+
 var oracleHost = process.env.ORACLE_HOST || 'localhost';
-var oraclePort = process.env.ORACLE_PORT || 1521;
+var oraclePort = process.env.ORACLE_PORT ? parseInt(process.env.ORACLE_PORT) : 1521;
+var oracleSID = process.env.ORACLE_SID || 'ORCLCDB';
 
 var oracleConnectSettings = {
   'password': process.env.ORACLE_SYSPASSWORD || 'manager1',
   'user': process.env.ORACLE_SYSUSER || 'sys',
-  'connectString': oracleHost + ':' + oraclePort + '/' + (process.env.ORACLE_SID || 'orclpdb.ad.infosys.com')
+  'connectString': oracleHost + ':' + oraclePort + '/' + oracleSID
 };
 
-var userName = process.env.CI_PROJECT_NAMESPACE.toUpperCase() + '-' + (process.env.CI_PROJECT_NAME || 'oecloud').toUpperCase();
-var password = process.env.CI_PROJECT_NAMESPACE.toLowerCase();
+var namespace = process.env.CI_PROJECT_NAMESPACE ? process.env.CI_PROJECT_NAMESPACE.replace(/[^a-zA-Z0-9]/g, '') : 'oecloudio';
+var name = process.env.CI_PROJECT_NAME ? process.env.CI_PROJECT_NAME.replace(/[^a-zA-Z0-9]/g, '') : 'oecloud';
+
+var userName = namespace.toUpperCase() + '_' + name.toUpperCase();
+var password = namespace.toLowerCase();
 
 var grants = [
   'CREATE VIEW',
@@ -44,30 +51,32 @@ var grants = [
 ];
 
 function createUser(connection, cb) {
-  var sql = 'alter session set "_ORACLE_SCRIPT"=true';
-  connection.execute(sql, function testFunction(e, r) {
-    if (e) {
-      console.error('Ignoring error of alter session. UserName : ' + userName + ' Error :' + e);
+  var alterSQL = 'alter session set "_ORACLE_SCRIPT"=true';
+  connection.execute(alterSQL, function (alterErr, alterRes) {
+    if (alterErr) {
+      console.error('Ignoring error of alter session. UserName : ' + userName + ' Error :' + alterErr);
     }
-    console.log(sql, ' ......... ok');
-    var sql = 'CREATE USER "' + userName + '" IDENTIFIED BY ' + password;
+    console.log(alterSQL, ' ......... ok');
 
-    connection.execute(sql, function testFunction(err, result) {
-      if (err) {
-        throw new Error('Unable to create user ' + userName + ' Error :' + err);
+    var createUserSQL = 'CREATE USER "' + userName + '" IDENTIFIED BY ' + password;
+    connection.execute(createUserSQL, function (createErr, createRes) {
+      if (createErr) {
+        console.error(createErr);
+        throw new Error('Unable to create user ' + userName);
       }
-      console.log(sql, ' ......... ok');
-      async.each(grants, function testFunction(g, callback) {
-        var sql = 'GRANT ' + g + ' to "' + userName + '"';
+      console.log(createUserSQL, ' ......... ok');
 
-        connection.execute(sql, function testFunction(err2, result2) {
-          if (err2) {
-            throw new Error('Unable to execute grant ' + sql);
+      async.each(grants, function (g, callback) {
+        var grantSQL = 'GRANT ' + g + ' to "' + userName + '"';
+        connection.execute(grantSQL, function (grantErr, grantRes) {
+          if (grantErr) {
+            console.error(grantErr);
+            throw new Error('Unable to execute grant ' + grantSQL);
           }
-          console.log(sql, ' ......... ok');
+          console.log(grantSQL, ' ......... ok');
           return callback();
         });
-      }, function testFunction(err) {
+      }, function (grantAsyncErr) {
         console.log('User ' + userName + ' Created successfully');
         return cb();
       });
@@ -75,78 +84,83 @@ function createUser(connection, cb) {
   });
 }
 
-
 function dropTables(cb) {
-  var oracleConnectSettings2 = Object.assign({}, oracleConnectSettings);
-  oracleConnectSettings2.user = userName;
-  oracleConnectSettings2.password = password;
+  var oracleUserConnectSettings = {
+    'password': password,
+    'user': userName,
+    'connectString': oracleHost + ':' + oraclePort + '/' + oracleSID
+  };
 
-  oracledb.getConnection(
-    oracleConnectSettings2,
-    function testFunction(err, connection) {
-      if (err) {
-        throw new Error('Unable to connect to Oracle Database ' + JSON.stringify(oracleConnectSettings));
+  oracledb.getConnection(oracleUserConnectSettings, function (userConnectionErr, connection) {
+    if (userConnectionErr) {
+      console.error(userConnectionErr);
+      throw new Error('Unable to connect to Oracle Database ' + JSON.stringify(oracleUserConnectSettings));
+    }
+
+    var totalRows = 1000;
+    var selectDropTableSQL = "select 'drop table \"' || table_name || '\"' from all_tables where owner = '" + userName + "'";
+    connection.execute(selectDropTableSQL, {}, { maxRows: totalRows }, function (selectDropErr, selectDropRes) {
+      if (selectDropErr) {
+        console.error(selectDropErr);
+        throw new Error('Unable to find tables ' + userName);
       }
-      var sql = "select 'drop table \"' || table_name || '\"' from all_tables where owner = '" + userName + "'";
-      var totalRows = 1000;
 
-      connection.execute(sql, {}, {maxRows: totalRows}, function testFunction(err, result) {
-        if (err) {
-          throw new Error('Unable to find tables ' + userName + ' Error :' + err);
-        }
-        connection.execute(sql, {}, {maxRows: totalRows}, function testFunction(err2, result2) {
-          if (err2) {
-            throw new Error('Unable to execute droping of table ' + sql);
+      if (!selectDropRes || !selectDropRes.rows || selectDropRes.rows.length === 0) {
+        return cb();
+      }
+
+      async.each(selectDropRes.rows, function (row, callback) {
+        var dropTableSQL = row[0];
+        connection.execute(dropTableSQL, function (dropTableErr, dropTableRes) {
+          if (dropTableErr) {
+            console.error(dropTableErr);
+            throw new Error('Unable to drop table\nSQL: ' + sql);
           }
-          if (!result2 || !result2.rows || result2.rows.length === 0) {
-            return cb();
-          }
-          async.each(result2.rows, function testFunction(row, callback) {
-            var sql = row[0];
-            connection.execute(sql, function testFunction(err2, result2) {
-              if (err2) {
-                throw new Error('Unable to drop table\nERROR : ' + err2 + '\nSQL : ' + sql);
-              }
-              console.log(sql, ' ......... ok');
-              return callback();
-            });
-          }, function testFunction(err) {
-            console.log('Tables of user ' + userName + ' dropped successfully');
-            return cb();
-          });
+          console.log(dropTableSQL, ' ......... ok');
+          return callback();
         });
+      }, function (dropAsyncErr) {
+        console.log('Tables of user ' + userName + ' dropped successfully');
+        return cb();
       });
     });
+  });
 }
 
-oracledb.getConnection(
-  oracleConnectSettings,
-  function testFunction(err, connection) {
-    if (err) {
-      throw new Error('Unable to connect to Oracle Database ' + JSON.stringify(oracleConnectSettings));
-    }
-    var sql = "select username, user_id from dba_users where username = '" + userName + "'";
-    console.log(sql);
-    connection.execute(sql,
-      function testFunction(err, result) {
-        if (err) {
-          console.error(err); return;
-        }
-        if (!result.rows || result.rows.length == 0) {
-          createUser(connection, function testFunction(err) {
-            if (err) {
-              return process.exit(1);
-            }
-            return process.exit();
-          });
-        } else {
-          dropTables(function testFunction(err) {
-            if (err) {
-              return process.exit(1);
-            }
-            return process.exit();
-          });
-        }
-      });
-  });
+function generateUserBundle() {
+  console.log("Generating oracle user details shell script");
+  fs.writeFileSync('./oracle-user.sh', '#!/bin/sh' + os.EOL);
+  fs.appendFileSync('./oracle-user.sh', 'export ORACLE_USERNAME=' + userName + os.EOL);
+  fs.appendFileSync('./oracle-user.sh', 'export ORACLE_PASSWORD=' + password + os.EOL);
+}
 
+oracledb.getConnection(oracleConnectSettings, function (connectionErr, connection) {
+  if (connectionErr) {
+    console.error(connectionErr);
+    throw new Error('Unable to connect to Oracle Database ' + JSON.stringify(oracleConnectSettings));
+  }
+  var sql = "select username, user_id from dba_users where username = '" + userName + "'";
+  console.log(sql);
+  connection.execute(sql, function (err, result) {
+    if (err) {
+      console.error(err); return;
+    }
+    if (!result.rows || result.rows.length == 0) {
+      createUser(connection, function (err) {
+        if (err) {
+          return process.exit(1);
+        }
+        generateUserBundle();
+        return process.exit();
+      });
+    } else {
+      dropTables(function (err) {
+        if (err) {
+          return process.exit(1);
+        }
+        generateUserBundle();
+        return process.exit();
+      });
+    }
+  });
+});
